965724ca2234ae9dde0ef91ee493e4167cf603cf
Luke Taylor <ldt@deentaylor.com>
2016-05-10 14:17:30 -0400

    Move to the 1000 most common words
    I get better, funnier sententences that way

---
+++ wordgen.py
@@ -0,0 +1,24 @@
+''' Build a word list of the 1000 most common words. I'm
+using the word list that was used in Randall Munroe's
+"Thing Explainer," and pulling it off xkcd.com '''
+
+import os
+
+import requests
+
+def getWords():
+    """ If the word list has not been downloaded, download
+    it. Then return the word list. """
+    # Word list has not been downloaded. Download, save, and return words.
+    if not os.path.exists("words.txt"):
+        r = requests.get("http://xkcd.com/simplewriter/words.js")
+        words = r.text.split('"')[1].split("|")
+        with open("words.txt", "wb") as f:
+            f.write("\n".join(words).encode("utf-8"))
+        return words
+    # Words list is saved, read and return.
+    with open("words.txt", "r") as f:
+        return [l.strip() for l in f.readlines()]
+    
+if __name__ == "__main__":
+    getWords()
