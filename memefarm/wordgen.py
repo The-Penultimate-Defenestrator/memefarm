983a317653648bc55e7e3eee74a8d2301cf1a91c
The-Penultimate-Defenestrator <ldt@deentaylor.com>
2016-05-10 21:31:12 -0400

    Change path details

--- memefarm/wordgen.py
+++ memefarm/wordgen.py
@@ -6,19 +6,26 @@ import os
 
 import requests
 
+localdir = os.path.dirname(__file__)
+
+
 def getWords():
     """ If the word list has not been downloaded, download
     it. Then return the word list. """
+
+    wordspath = os.path.join(localdir, "words.txt")
+
     # Word list has not been downloaded. Download, save, and return words.
-    if not os.path.exists("words.txt"):
+    if not os.path.exists(wordspath):
         r = requests.get("http://xkcd.com/simplewriter/words.js")
         words = r.text.split('"')[1].split("|")
-        with open("words.txt", "wb") as f:
+        with open(wordspath, "wb") as f:
             f.write("\n".join(words).encode("utf-8"))
         return words
     # Words list is saved, read and return.
-    with open("words.txt", "r") as f:
+    with open(wordspath, "r") as f:
         return [l.strip() for l in f.readlines()]
-    
+
+
 if __name__ == "__main__":
     getWords()
